dea076537856724c88ce2e37fc6a0eab3e26d61e
= <=>
2021-07-07 14:43:25 +0300

    Install

---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+sudo apt update -y && sudo apt upgrade -y
+
+sudo apt install -y git curl fzf gcc make ruby-full
+
+sudo gem install colorls
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+cp ./.p10k.zsh ./.zshrc ~
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
