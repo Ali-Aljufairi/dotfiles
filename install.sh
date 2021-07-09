d28bdf45b1699c4f63197ad96fb2d1ec249c766f
HUSSPIN <huss4in77@gmail.com>
2021-07-09 15:33:42 +0300

    install

--- install.sh
+++ install.sh
@@ -1,16 +1,13 @@
 #!/bin/zsh
 
-sudo apt update -y && sudo apt upgrade -y
-
-sudo apt install -y git curl fzf gcc make ruby-full
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y zsh git curl fzf gcc make ruby-full
 
 sudo gem install colorls
 
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
-cp ./.p10k.zsh ./.zshrc ~
-
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp ~/dotfiles/.p10k.zsh ~/dotfiles/.zshrc ~
